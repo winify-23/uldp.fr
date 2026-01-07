c671656fbfffdf5355ac2e9db7b9de017ad32c8a
Wini_Fy <thanh.trandinh205@gmail.com>
2026-01-07 16:34:12 +0700

    feat: Update the importance of occupational standards page with layout adjustments and link corrections

--- class/_scripts/scrollto.js
+++
@@ -1,7 +0,0 @@
-        $(document).ready(function (){
-            $("#scrollTo").click(function (){
-                $('html, body').animate({
-                    scrollTop: $(".site-name").offset().top
-                }, 1000);
-            });
-        });
